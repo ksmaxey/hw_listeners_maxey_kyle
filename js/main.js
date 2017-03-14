7096422ac027f756dd10bbeaf56f3fd71ac88370
Kyle Maxey <ksmaxey@gmail.com>
2017-03-14 16:34:41 -0400

    work on click number
    The click number is console logged and doesn't change HTML, need to
    correct this before submitting assignment.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ var clicks = 0;
 
 header.addEventListener('click', function () {
     var paragraph = document.createElement('paragraph');
-    paragraph.innerHTML = '<p>This is click number x</p>';
-    document.getElementById('h1').appendChild(paragraph, ++clicks);
+    paragraph.innerHTML = '<p>This is click number</p>';
+    document.getElementById('h1').appendChild(paragraph);
 });
 
 var count = document.getElementById('h1');
 count.onclick = function () {
-    console.log(+clicks);
+    console.log(++clicks);
 };
