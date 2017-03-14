0c9d2381c06895b276f0167c72f19f729ffd4332
Kyle Maxey <ksmaxey@gmail.com>
2017-03-14 19:16:28 -0400

    add submission event listener
    Add event listeners so input values will be logged to the console.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Clicks
+
 var header = document.getElementById('h1');
 var clicks = 0;
 
@@ -11,3 +13,29 @@ var count = document.getElementById('h1');
 count.onclick = function () {
     console.log(++clicks);
 };
+
+//Submitted Info
+
+var firstNameInput = document.getElementById('firstName');
+document.addEventListener('submit', function (info) {
+    info.preventDefault();
+    console.log('The firstName is:', firstNameInput.value);
+});
+
+var lastNameInput = document.getElementById('lastName');
+document.addEventListener('submit', function (info) {
+    info.preventDefault();
+    console.log('The lastName is:', lastNameInput.value);
+});
+
+var emailInput = document.getElementById('email');
+document.addEventListener('submit', function (info) {
+    info.preventDefault();
+    console.log('The email is:', emailInput.value);
+});
+
+var messageInput = document.getElementById('message');
+document.addEventListener('submit', function (info) {
+    info.preventDefault();
+    console.log('The message is:', messageInput.value);
+});
