d3d752943504fb65ad8825f27e036001513b31ff
Kyle Maxey <ksmaxey@gmail.com>
2017-03-14 21:51:43 -0400

    fix click number issue
    Fixed issue with the click counter only showing in the console.  Click
    counts now accurately reflect in the HTML.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
+//Clicks
+
 var header = document.getElementById('h1');
 var clicks = 0;
 
 header.addEventListener('click', function () {
     var paragraph = document.createElement('paragraph');
-    paragraph.innerHTML = '<p>This is click number</p>';
+    paragraph.innerHTML = '<p>This is click number</p>' + (++clicks);
     document.getElementById('h1').appendChild(paragraph);
 });
 
-var count = document.getElementById('h1');
-count.onclick = function () {
-    console.log(++clicks);
-};
+//Submitted Info
+
+var firstNameInput = document.getElementById('firstName');
+var lastNameInput = document.getElementById('lastName');
+var emailInput = document.getElementById('email');
+var messageInput = document.getElementById('message');
+document.addEventListener('submit', function (info) {
+    info.preventDefault();
+    console.log('The firstName is:', firstNameInput.value);
+    console.log('The lastName is:', lastNameInput.value);
+    console.log('The email is:', emailInput.value);
+    console.log('The message is:', messageInput.value);
+});
