4d9898ea3825de411644c99e77b36b379ff10299
Kyle Maxey <ksmaxey@gmail.com>
2017-03-14 21:56:43 -0400

    correct click count on custom branch

--- js/main.js
+++ js/main.js
@@ -5,14 +5,9 @@ var clicks = 0;
 
 header.addEventListener('click', function () {
     var paragraph = document.createElement('paragraph');
-    paragraph.innerHTML = '<p>This is click number</p>';
+    paragraph.innerHTML = '<p>This is click number</p>' + (++clicks);
     document.getElementById('h1').appendChild(paragraph);
 });
-
-var count = document.getElementById('h1');
-count.onclick = function () {
-    console.log(++clicks);
-};
 
 //Submitted Info
 
